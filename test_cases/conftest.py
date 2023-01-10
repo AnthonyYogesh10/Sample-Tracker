6c2c5ed4b7f59ce7dc4f2501cb510f924e8d0f87
AnthonyYogesh10 <anthonyyogesh10@gmail.com>
2023-01-10 20:35:41 +0530

    screenshot added in conftest

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,3 +1,4 @@
+import os
 import time
 import pytest
 
@@ -15,3 +16,28 @@ def setup(request):
  request.cls.driver = driver
  yield
  driver.close()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    driver = webdriver.Chrome()
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        # always add url to report
+        extra.append(pytest_html.extras.url("https://keycloak-gbs-dev.trutesta.io/auth/realms/Intertek/protocol/openid-connect/auth?client_id=trutesta&redirect_uri=https%3A%2F%2Fintertek-dev.trutesta.io%2Ftrusamples.mdb5%2Fshipment-types%2Fb1eccf7f-ae34-4477-85d9-eee1aa138dae%2FshipmentDetails&state=48c7d42d-57c8-4b2d-ba42-634f981935d8&response_mode=fragment&response_type=code&scope=openid&nonce=23743122-f53b-4602-9d51-bcd151a2c6ad"))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            report_directory = os.path.dirname(item.config.option.htmlpath)
+            file_name = str(int(round(time.time() * 1000))) + ".png"
+            # file_name = report.nodeid.replace("::", "_") + ".png"
+            destinationFile = os.path.join(report_directory, file_name)
+            driver.save_screenshot(destinationFile)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:300px;height=200px" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>'%file_name
+            extra.append(pytest_html.extras.html("html"))
+        report.extra = extra
