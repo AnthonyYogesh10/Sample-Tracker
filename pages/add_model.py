35226d792dd0129981742f50b000151981540797
AnthonyYogesh10 <anthonyyogesh10@gmail.com>
2023-01-25 20:41:45 +0530

    xpath in add_model and home_page are updated

--- pages/add_model.py
+++ pages/add_model.py
@@ -2,46 +2,74 @@ from selenium.webdriver.common.by import By
 
 
 class Add_model():
-    def __init__(self,driver):
+    def __init__(self, driver):
         self.driver = driver
 
-    def name_input(self,input_value):
-        name = self.driver.find_element(By.XPATH,"//input[@id='shipment-type-name']")
-        name.clear()
-        name.send_keys(input_value)
+    name_input_field = "//input[@id='shipment-type-name']"
+    description_testbox_field = "//input[@id='shipment-type-name']"
+    bussiness_code_input_field = "//input[@id='businessCode']"
+    item_status_inactive_field = "//input[@id='deliverable-item-status']"
+    submit_btn_field = "//body[1]/div[1]/div[2]/div[1]/mdb-modal-container[1]/div[1]/div[1]/add-shipment-type-modal[1]/div[3]/button[1]"
+    add_toaster_field = "//div[@class='toast-body bg-light']"
+    cancel_btn_field = "//button[normalize-space()='Cancel']"
 
-    def description_testbox(self,description_value):
-        description = self.driver.find_element(By.XPATH,"//textarea[@id='shipment-type-description']")
-        description.clear()
-        description.send_keys(description_value)
+    def get_name_input_field(self):
+        return self.driver.find_element(By.XPATH, self.name_input_field)
 
-    def bussiness_code_input(self,input_value):
-        bussiness_code = self.driver.find_element(By.XPATH,"//input[@id='businessCode']")
-        bussiness_code.clear()
-        bussiness_code.send_keys(input_value)
+    def get_description_field(self):
+        return self.driver.find_element(By.XPATH, self.description_testbox_field)
+
+    def get_bussiness_code_input_field(self):
+        return self.driver.find_element(By.XPATH, self.bussiness_code_input_field)
+
+    def get_item_status_inactive_field(self):
+        return self.driver.find_element(By.XPATH, self.item_status_inactive_field)
+
+    def get_submit_btn_field(self):
+        return self.driver.find_element(By.XPATH, self.submit_btn_field)
+
+    def get_add_toaster_field(self):
+        return self.driver.find_element(By.XPATH, self.add_toaster_field)
+
+    def get_cancel_btn_field(self):
+        return self.driver.find_element(By.XPATH, self.cancel_btn_field)
+
+    def name_input(self, input_value):
+        self.get_name_input_field().clear()
+        self.get_name_input_field().send_keys(input_value)
+
+    def description_testbox(self, description_value):
+        self.get_description_field().clear()
+        self.get_description_field().send_keys(description_value)
+
+    def bussiness_code_input(self, input_value):
+        self.get_bussiness_code_input_field().clear()
+        self.get_bussiness_code_input_field().send_keys(input_value)
 
     def item_status_inactive(self):
-        item_status = self.driver.find_element(By.XPATH,"//input[@id='deliverable-item-status']")
-        item_status.click()
+        self.get_item_status_inactive_field().click()
 
     def submit_btn(self):
-        self.driver.find_element(By.XPATH,"//body[1]/div[1]/div[2]/div[1]/mdb-modal-container[1]/div[1]/div[1]/add-shipment-type-modal[1]/div[3]/button[1]").click()
-
-    def add_toaster(self,input_name):
-        success_value=self.driver.find_element(By.XPATH,"//div[@class='toast-body bg-light']").text
-        # print(success_value)
-        s1 = input_name
-        s2 = " Is Added"
-        s3 = s1 + s2
-        print(s3)
-        # print(success_value)
-        #to verify the heading is equal to input value
-        heading = self.driver.find_element(By.XPATH,"//span[normalize-space()='new sample2']")
-        heading_text = heading.text
-        if success_value == s3 and heading_text == input_name:
-           print("testcase add passed")
-        else:
-            assert "testcase add failed"
+        self.get_submit_btn_field().click()
 
     def cancel_btn(self):
-        self.driver.find_element(By.XPATH,"//button[normalize-space()='Cancel']").click()
+        self.get_cancel_btn_field().click()
+
+        def add_toaster(self, input_name):
+            success_value = self.get_add_toaster_field().text
+            # print(success_value)
+            s1 = input_name
+            s2 = " Is Added"
+            s3 = s1 + s2
+            print(s3)
+            # print(success_value)
+            # to verify the heading is equal to input value
+            heading = self.driver.find_element(By.XPATH, "//span[normalize-space()='new sample2']")
+            heading_text = heading.text
+            if success_value == s3 and heading_text == input_name:
+                print("testcase add passed")
+            else:
+                assert "testcase add failed"
+
+
+from selenium.webdriver.common.by import By
--- pages/home_page.py
+++ pages/home_page.py
@@ -4,58 +4,82 @@ from selenium.webdriver.common.by import By
 class HomePage():
     def __init__(self,driver):
         self.driver = driver
+   #Locators (it is used to change url easily and essy to maintain)
+    menubar_field = "//i[@class='fas fa-bars fa-2x']"
+    side_nav_options_field = "//ul[@id='scroll-container']/mdb-sidenav-item/li/div/a"
+    side_nav_admin_drdw_field = "//ul[@class='sidenav-collapse collapse show']/li"
+    under_categories_field = "//mdb-sidenav-item[@class='ng-star-inserted']//li[@class='sidenav-item']/div/a"
+    add_button_field = "//button[normalize-space()='Add']"
+    modify_btn_field = "//button[normalize-space()='Modify']"
+    delete_btn_field = "//button[normalize-space()='Delete']"
+    search_input_field = "//input[@id='quick-search']"
+    search_button_field = "//i[@class='fa fa-search']"
+
+  # Loactors to Use (It is used to avoid store variables
+  #                 eg: name = xpath
+  #                     name.click )
+    def get_menubar_field(self):
+        return self.driver.find_element(By.XPATH,self.menubar_field)
+    def get_side_nav_options_field(self):
+        return self.driver.find_elements(By.XPATH,self.side_nav_options_field)
+    def get_side_nav_admin_drdw_field(self):
+        return self.driver.find_elements(By.XPATH,self.side_nav_admin_drdw_field)
+    def get_under_categories_field(self):
+        return self.driver.find_elements(By.XPATH,self.under_categories_field)
+    def get_add_button_field(self):
+        return self.driver.find_element(By.XPATH,self.add_button_field)
+    def get_modify_btn_field(self):
+        return self.driver.find_element(By.XPATH,self.modify_btn_field)
+    def get_delete_btn_field(self):
+        return self.driver.find_element(By.XPATH,self.delete_btn_field)
+    def get_search_input_field(self):
+        return self.driver.find_element(By.XPATH,self.search_input_field)
+    def get_search_button_field(self):
+        return self.driver.find_element(By.XPATH,self.search_button_field)
+
 
     def menu_bar(self):
-        menubar =self.driver.find_element(By.XPATH,"//i[@class='fas fa-bars fa-2x']")
-        menubar.click()
+        self.get_menubar_field().click()
 
     def side_nav(self,nav_options):
-        side_nav =self.driver.find_elements(By.XPATH,"//ul[@id='scroll-container']/mdb-sidenav-item/li/div/a")
+        side_nav = self.get_side_nav_options_field()
         for option in side_nav:
             if option.text == nav_options:
                 option.click()
 
     def side_nav_admin_drdw(self,dropdown_options):
-         administration_dropdown = self.driver.find_elements(By.XPATH,"//ul[@class='sidenav-collapse collapse show']/li")
-         for dropdown_option in administration_dropdown:
+         administration_dropdown = self.get_side_nav_admin_drdw_field()
+         for dropdown_option in administration_dropdown: #change it to utlities folder
              if dropdown_option.text == dropdown_options:
                  dropdown_option.click()
 
     def under_categories(self,category_option):
-        category_dropdown = self.driver.find_elements(By.XPATH,"//mdb-sidenav-item[@class='ng-star-inserted']//li[@class='sidenav-item']/div/a")
-        for dropdown in category_dropdown:
+        category_dropdown = self.get_under_categories_field()
+        for dropdown in category_dropdown: #change it to utlities folder
             if dropdown.text == category_option:
                 dropdown.click()
 
     def add_button(self):
-         add =self.driver.find_element(By.XPATH,"//button[normalize-space()='Add']")
-         add.click()
-
-    def data_of_sample_type(self):
-        datas = self.driver.find_elements(By.XPATH,"//div[@id='mid-list']/a")
-        lenth_of_data = len(datas)
-        # for data in datas:
+         self.get_add_button_field().click()
 
     def modify_btn(self):
-        modify = self.driver.find_element(By.XPATH,"//button[normalize-space()='Modify']")
-        modify.click()
+        self.get_modify_btn_field().click()
 
     def delete_btn(self):
-        delete = self.driver.find_element(By.XPATH,"//button[normalize-space()='Delete']")
-        delete.click()
+        self.get_delete_btn_field().click()
 
     def search_input(self,search_value):
-        print(search_value)
-        search = self.driver.find_element(By.XPATH,"//input[@id='quick-search']")
-        search.send_keys(search_value)
+         self.get_search_input_field().send_keys(search_value)
 
     def search_button(self):
-        search_btn = self.driver.find_element(By.XPATH,"//i[@class='fa fa-search']")
-        search_btn.click()
+         self.get_search_button_field().click()
 
     def fetch_url(self):
         new_url= self.driver.current_url
         return new_url
 
-
+    def data_of_sample_type(self):
+        datas = self.driver.find_elements(By.XPATH,"//div[@id='mid-list']/a")
+        lenth_of_data = len(datas)
+        # for data in datas:
 
